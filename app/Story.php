bdb6f7d8872a2c39c593c43b6c4437897e992c1d
98Marvin <marvinwanyama99@gmail.com>
2022-08-30 16:48:51 +0300

    Eloquent Models and Relationships

--- app/Story.php
+++ app/Story.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
-    //
+    protected $fillable = [
+        // 'user_id',
+        'title',
+        'body',
+        'genre',
+        'status'
+    ];
+    
+    public function user () {
+        return $this->belongsTo(\App\User::class);
+    }
 }
